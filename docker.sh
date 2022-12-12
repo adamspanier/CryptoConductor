b3d2c08709d003b4c83436fcb3964fb2c7cc8356
Adam Spanier <aspanier@unomaha.edu>
2022-12-12 11:25:46 -0600

    Docker shell repairs.

--- docker.sh
+++ docker.sh
@@ -4,6 +4,7 @@
 	#sudo docker run docker-django-v0.0
 	sudo apt install docker
 	sudo apt install docker.io
+	sudo pip install docker-compose
 	sudo docker compose down
 	sudo docker compose build
 	sudo docker compose up
