f4f27c794560a0513ba1a4b6f51929d662b73079
Adam Spanier <aspanier@unomaha.edu>
2022-12-12 11:08:15 -0600

    Docker shell corrections

--- docker.sh
+++ docker.sh
@@ -2,6 +2,8 @@
 
 	#sudo docker build . -t docker-django-v0.0
 	#sudo docker run docker-django-v0.0
+	sudo apt install docker
+	sudo apt install docker.io
 	sudo docker compose down
 	sudo docker compose build
 	sudo docker compose up
